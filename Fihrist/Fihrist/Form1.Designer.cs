cf5ce927ed41737ca91486ce8ce40f1ae4c2123c
Selin Güzel <76588842+selinguz@users.noreply.github.com>
2021-02-21 16:40:24 +0300

    Fihrist Uygulaması
    Fihrist Uygulaması

---
+++ Fihrist/Fihrist/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Fihrist
+{
+    partial class Kisiler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Kisiler));
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblAdres = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.rtxtAdress = new System.Windows.Forms.RichTextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.dgwList = new System.Windows.Forms.DataGridView();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblAd.Location = new System.Drawing.Point(172, 45);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(35, 13);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad:";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(172, 71);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(40, 13);
+            this.lblSoyad.TabIndex = 1;
+            this.lblSoyad.Text = "Soyad:";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(172, 95);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(46, 13);
+            this.lblTelefon.TabIndex = 2;
+            this.lblTelefon.Text = "Telefon:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(172, 119);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblAdres
+            // 
+            this.lblAdres.AutoSize = true;
+            this.lblAdres.Location = new System.Drawing.Point(172, 147);
+            this.lblAdres.Name = "lblAdres";
+            this.lblAdres.Size = new System.Drawing.Size(37, 13);
+            this.lblAdres.TabIndex = 4;
+            this.lblAdres.Text = "Adres:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(224, 42);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(161, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(224, 68);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(161, 20);
+            this.txtSurname.TabIndex = 6;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(224, 92);
+            this.txtPhone.MaxLength = 10;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(161, 20);
+            this.txtPhone.TabIndex = 7;
+            this.txtPhone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhone_KeyPress);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(224, 115);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(161, 20);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // rtxtAdress
+            // 
+            this.rtxtAdress.Location = new System.Drawing.Point(224, 144);
+            this.rtxtAdress.Name = "rtxtAdress";
+            this.rtxtAdress.Size = new System.Drawing.Size(161, 72);
+            this.rtxtAdress.TabIndex = 9;
+            this.rtxtAdress.Text = "";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(408, 16);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(104, 32);
+            this.btnKaydet.TabIndex = 10;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Location = new System.Drawing.Point(408, 57);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(104, 32);
+            this.BtnSil.TabIndex = 11;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(408, 144);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(104, 32);
+            this.btnList.TabIndex = 12;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // dgwList
+            // 
+            this.dgwList.AllowUserToOrderColumns = true;
+            this.dgwList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgwList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwList.Location = new System.Drawing.Point(76, 234);
+            this.dgwList.Name = "dgwList";
+            this.dgwList.Size = new System.Drawing.Size(532, 179);
+            this.dgwList.TabIndex = 13;
+            this.dgwList.SelectionChanged += new System.EventHandler(this.dgwList_SelectionChanged);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(172, 19);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(19, 13);
+            this.lblId.TabIndex = 14;
+            this.lblId.Text = "Id:";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(224, 16);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(161, 20);
+            this.txtId.TabIndex = 15;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(408, 100);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(104, 32);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(408, 185);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(104, 35);
+            this.btnTemizle.TabIndex = 17;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // Kisiler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(688, 450);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.dgwList);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.rtxtAdress);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblAdres);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblAd);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Kisiler";
+            this.Text = "Kişiler ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblAdres;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.RichTextBox rtxtAdress;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.DataGridView dgwList;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnTemizle;
+    }
+}
+
---
+++ Fihrist/Fihrist/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fihrist
+{
+    public partial class Kisiler : Form
+    {
+        public Kisiler()
+        {
+            InitializeComponent();
+        }
+        
+       
+        private void GetList()
+        {
+            //SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB; Database=Fihrist");
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Kisiler", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            dgwList.DataSource = dataTable;
+
+        }
+
+        SqlConnection connection;
+        private void GetSettings()
+        {
+            IniFile file = new IniFile(Application.StartupPath + @"\Ayarlar.INI");
+            string serverName = file.Read("ServerName", "SQL");
+            string databaseName = file.Read("DatabaseName", "SQL");
+            string userName = file.Read("UserName", "SQL");
+            string password = file.Read("Password", "SQL");
+            connection = new SqlConnection(@"Server=" + serverName + "; Database=" + databaseName + "; UID=" + userName + "; password=" + password);
+            connection.Open();
+        }
+
+        private void UpdateAll()
+        {
+            //SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB; Database=Fihrist");
+
+
+            SqlCommand command = new SqlCommand("UPDATE Kisiler SET Name=@Name, Surname=@Surname, PhoneNumber=@PhoneNumber, Email=@Email, Adress=@Adress WHERE Id=@Id", connection);
+            command.Parameters.AddWithValue("@Name", txtName.Text);
+            command.Parameters.AddWithValue("@Surname", txtSurname.Text);
+            command.Parameters.AddWithValue("@PhoneNumber", txtPhone.Text);
+            command.Parameters.AddWithValue("@Email", txtEmail.Text);
+            command.Parameters.AddWithValue("@Adress", rtxtAdress.Text);
+            command.Parameters.AddWithValue("@Id", txtId.Text);
+
+
+            DialogResult cikis = new DialogResult();
+            cikis = MessageBox.Show("Kayıt güncellenecek. Emin misiniz?", "Uyarı", MessageBoxButtons.YesNo);
+            if (cikis == DialogResult.Yes)
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Güncellendi", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                command.Cancel();
+            }
+            GetList();
+        }
+
+        private void ClearAll()
+        {
+            txtId.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtSurname.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            rtxtAdress.Text = string.Empty;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetSettings();
+            GetList();
+            ClearAll();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            GetList();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            //SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB; Database=Fihrist");
+            //connection.Open();
+            SqlCommand command = new SqlCommand("INSERT INTO Kisiler (Name,Surname,PhoneNumber,Email,Adress) VALUES(@Name,@Surname,@PhoneNumber,@Email,@Adress)", connection);
+         
+            command.Parameters.AddWithValue("@Name", txtName.Text);
+            command.Parameters.AddWithValue("@Surname", txtSurname.Text);
+            command.Parameters.AddWithValue("@PhoneNumber", txtPhone.Text);
+            command.Parameters.AddWithValue("@Email", txtEmail.Text);
+            command.Parameters.AddWithValue("@Adress", rtxtAdress.Text);
+
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Ad Alanı boş olamaz!", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                command.Cancel();
+                txtName.Text = string.Empty;
+            }
+            else if (txtSurname.Text == "")
+            {
+                MessageBox.Show("Soyad Alanı boş olamaz!", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                command.Cancel();
+                txtSurname.Text = string.Empty;
+            }
+            else if (txtEmail.Text == "" || !txtEmail.Text.Contains("@"))
+            {
+                MessageBox.Show("E-mail Alanı boş veya hatalı!", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                command.Cancel();
+                txtEmail.Text = string.Empty;
+            }
+            else if (txtPhone.Text == "")
+            {
+                MessageBox.Show("Telefon Alanı boş", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                command.Cancel();
+                txtPhone.Text = string.Empty;
+            }
+            else
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Kaydedildi", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearAll();
+                GetList();
+            }
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            //SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB; Database=Fihrist");
+            //connection.Open();
+            SqlCommand command = new SqlCommand("DELETE FROM Kisiler WHERE Id=@Id", connection);
+            command.Parameters.AddWithValue("@Id", txtId.Text);
+
+            DialogResult cikis = new DialogResult();
+            cikis = MessageBox.Show("Kayıt silinecek. Emin misiniz?", "Uyarı", MessageBoxButtons.YesNo);
+            if (cikis == DialogResult.Yes)
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Kayıt Silindi", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                command.Cancel();
+            }
+            GetList();
+            //connection.Close();
+
+        }
+
+        private void dgwList_SelectionChanged(object sender, EventArgs e)
+        {
+            txtId.Text = dgwList.CurrentRow.Cells[0].Value.ToString();
+            txtName.Text = dgwList.CurrentRow.Cells[1].Value.ToString();
+            txtSurname.Text = dgwList.CurrentRow.Cells[2].Value.ToString();
+            txtPhone.Text = dgwList.CurrentRow.Cells[3].Value.ToString();
+            txtEmail.Text = dgwList.CurrentRow.Cells[4].Value.ToString();
+            rtxtAdress.Text = dgwList.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text==""||txtPhone.Text==""||txtSurname.Text==""||txtEmail.Text=="")
+            {
+                MessageBox.Show("Tüm alanlar doldurulmalı", "Message Box", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                UpdateAll();
+            } 
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void txtPhone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+}
